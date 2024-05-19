482c1b8b7d0a7598c3d27179ffc1a135c83c7420
Christian Bonnell <christian.bonnell@gmail.com>
2024-05-19 09:14:02 -0500

    Modify eval utility for multiple models.

--- modeling_notebooks/modeling_notebooks/resnet_selection/model_eval_utilities.py
+++ modeling_notebooks/modeling_notebooks/resnet_selection/model_eval_utilities.py
@@ -3,12 +3,13 @@ import pandas as pd
 
 def generate_metric_curves(
     df: pd.DataFrame,
+    model_colname: str = "model_name",
     true_category_colname: str = "category",
     score_colname: str = "score",
     breaks: int = 100,
 ) -> pd.DataFrame:
     """
-    Generate a DataFrame with the ROC and PR curves for the given data.
+    Generate a DataFrame with the ROC and PR curves for each model.
 
     Yes, I know that scikit-learn has utilities for this, but thoe utilities don't
     provide the flexibility I would like for this project.
@@ -25,36 +26,42 @@ def generate_metric_curves(
 
     metric_results_records = []
 
-    for i in range(breaks + 1):
-        threshold = i / breaks
+    for model_name in df[model_colname].unique():
+        model_df = df[df[model_colname] == model_name].copy()
 
-        # Compute the confusion matrix
-        df["prediction"] = df[score_colname] > threshold
-        confusion_matrix = pd.crosstab(df[true_category_colname], df["prediction"])
+        for i in range(breaks + 1):
+            threshold = i / breaks
 
-        # Compute the true positive rate, false positive rate, precision, and recall
-        true_positive = confusion_matrix.loc[True, True]
-        false_positive = confusion_matrix.loc[False, True]
-        false_negative = confusion_matrix.loc[True, False]
+            # Compute the confusion matrix
+            model_df["prediction"] = model_df[score_colname] > threshold
+            confusion_matrix = pd.crosstab(
+                model_df[true_category_colname], model_df["prediction"]
+            )
 
-        true_positive_rate = true_positive / (true_positive + false_negative)
-        false_positive_rate = false_positive / (
-            false_positive + confusion_matrix.loc[False, False]
-        )
+            # Compute the true positive rate, false positive rate, precision, and recall
+            true_positive = confusion_matrix.loc[True, True]
+            false_positive = confusion_matrix.loc[False, True]
+            false_negative = confusion_matrix.loc[True, False]
 
-        precision = true_positive / (true_positive + false_positive)
-        recall = true_positive / (true_positive + false_negative)
+            true_positive_rate = true_positive / (true_positive + false_negative)
+            false_positive_rate = false_positive / (
+                false_positive + confusion_matrix.loc[False, False]
+            )
 
-        metric_results_records.append(
-            {
-                "threshold": threshold,
-                "true_positive_count": true_positive,
-                "false_positive_count": false_positive,
-                "true_positive_rate": true_positive_rate,
-                "false_positive_rate": false_positive_rate,
-                "precision": precision,
-                "recall": recall,
-            }
-        )
+            precision = true_positive / (true_positive + false_positive)
+            recall = true_positive / (true_positive + false_negative)
 
-        return pd.DataFrame.from_records(metric_results_records)
+            metric_results_records.append(
+                {
+                    "model_name": model_name,
+                    "threshold": threshold,
+                    "true_positive_count": true_positive,
+                    "false_positive_count": false_positive,
+                    "true_positive_rate": true_positive_rate,
+                    "false_positive_rate": false_positive_rate,
+                    "precision": precision,
+                    "recall": recall,
+                }
+            )
+
+    return pd.DataFrame.from_records(metric_results_records)
