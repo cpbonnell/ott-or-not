c78960294247f2522c3ce5ed131a634b1629859f
Christian Bonnell <christian.bonnell@gmail.com>
2024-05-26 12:25:26 -0500

    Additional comments.

--- oon_utilities/oon_utilities/curator/image_repository.py
+++ oon_utilities/oon_utilities/curator/image_repository.py
@@ -3,24 +3,35 @@ These classes are utilities for collecting, managing, and curating collections
 of images for use in machine learning applications.
 """
 
-from abc import ABC
-from PIL import Image
 import hashlib
+from abc import ABC
 from importlib import resources
+
+from PIL import Image
 
 
 class ImageHasher:
     """
-    This class is a singleton that provides a method for hashing images.
+    This class is a singleton that provides methods for hashing images.
+
+    Some of the optional components of the class are ~ 10kb to 15kb in size,
+    so they are lazily loaded only if used, and the class is a singleton to
+    prevent multiple copies from being loaded into memory.
+
+    The hexdigest is the MD5 hash of the image, and the hashwords are two words
+    chosen from a list of adjectives and nouns, based on the hexdigest. The words
+    can themselves be used as a type of hash, and should be stable across multiple
+    runs of the program, as long as the version of the library (and hence the
+    included lists of adjectives and nouns) remains the same.
     """
 
-    instance: "ImageHasher" = None
+    _instance: "ImageHasher" = None
 
     @staticmethod
     def get_or_create() -> "ImageHasher":
-        if ImageHasher.instance is None:
+        if ImageHasher._instance is None:
             ImageHasher.instance = ImageHasher()
-        return ImageHasher.instance
+        return ImageHasher._instance
 
     def __init__(self):
         self._adjectives_resource = (
@@ -80,4 +91,11 @@ class ImageRepository(ABC):
 
 
 class FileSystemImageRepository(ImageRepository):
+    """
+    A simple image repository using only the local file system.
+
+    This class is an implementation of the ImageRepository ABC that stores images
+    on the local file system.
+    """
+
     pass
