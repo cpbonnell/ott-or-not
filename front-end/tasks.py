d475d60ba419bf3b4df7a9f880e4e63f3cd993ae
christianbonnell <christian.bonnell@gmail.com>
2025-06-03 17:36:54 -0500

    feat: first pass at a register and push task

--- front-end/tasks.py
+++ front-end/tasks.py
@@ -1,6 +1,14 @@
 from invoke import task, Collection, Context
 import io
 import aws
+import tomllib
+
+with open("pyproject.toml", "rb") as f:
+    pyproject = tomllib.load(f)
+
+package_version = pyproject["project"]["version"]
+# service_name = pyproject["project"]["name"]
+service_name = "oon/front-end"
 
 
 @task()
@@ -9,8 +17,11 @@ def down(ctx):
 
 
 @task(down)
-def build(ctx):
-    ctx.run("docker compose build")
+def build(ctx, tag: str | None = None):
+    new_env = {}
+    if tag:
+        new_env["TAG"] = tag
+    ctx.run("docker compose build", env=new_env)
 
 
 @task(build)
@@ -41,6 +52,26 @@ def login(ctx: Context, aws_profile: str = None):
     print(f"Docker has been logged in: {result.stdout}")
 
 
+@task(aws.profile, aws.region, aws.account_id)
+def register(ctx: Context):
+    # NOTE: this only works if a repository with the expected name already exists
+
+    print(f"Step 1 - Building the image and tagging with {package_version}")
+    build(ctx, tag=package_version)
+
+    print("Step 2 - Beginning login process...")
+    login(ctx)
+
+    print("Step 3 - Tag the image")
+    endpoint = f"{ctx.aws.account_id}.dkr.ecr.{ctx.aws.region}.amazonaws.com"
+    source_image = f"{service_name}:{package_version}"
+    destination_image = f"{endpoint}/{source_image}"
+    result = ctx.run(f"docker tag {source_image} {destination_image}")
+
+    print("Step 4 - Push the image to AWS ECR")
+    result = ctx.run(f"docker push {destination_image}")
+
+
 # ===== Overhead =====
-ns = Collection(down, build, up, login)
+ns = Collection(down, build, up, login, register)
 ns.add_collection(aws)
